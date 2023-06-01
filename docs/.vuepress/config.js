a7331e1137ffcc187b74f5fc7e963da10f66c331
luxurong <1074455781@qq.com>
2023-06-01 14:03:27 +0800

    功能文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,67 +5,70 @@
  * @LastEditTime: 2023-03-24 14:28:52
  */
 module.exports = {
-  title: '',
-  description: '这是fluxmq的官方文档',
-  dest: 'public',
-  base: '/',
-  head: [
-    ['link', { rel: 'icon', href: '/img/logo.svg' }],
-    ['script', { src: '/js/iframeSizer.contentWindow.min.js' }]
-  ],
-  port: 8082,
-  themeConfig: {
-    nav: [],
-    sidebar: [
-      {
-        title: "FluxMQ",
-        collapsable: true,
-        sidebarDepth: 1,
-        children: [
-          ['/views/product/introduction', '产品介绍'],
-          ['/views/product/core', '功能概览']
+    title: '',
+    description: '这是fluxmq的官方文档',
+    dest: 'public',
+    base: '/',
+    head: [
+        ['link', {rel: 'icon', href: '/img/logo.svg'}],
+        ['script', {src: '/js/iframeSizer.contentWindow.min.js'}]
+    ],
+    port: 8082,
+    themeConfig: {
+        nav: [],
+        sidebar: [
+            {
+                title: "FluxMQ",
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                    ['/views/product/introduction', '产品介绍'],
+                    ['/views/product/core', '功能概览']
+                ]
+            },
+            {
+                title: "安装部署",
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                    ['/views/install/optimize', '系统优化'],
+                    ['/views/install/deployment', '部署']
+                ]
+            },
+            ['/views/guide', '配置指南'],
+            {
+                title: "功能",
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                    ['/views/function/dashboard', 'Dashboard'],
+                    ['/views/function/auth', '认证鉴权'],
+                    ['/views/function/control', '访问控制'],
+                    ['/views/function/link', '连接管理'],
+                    ['/views/function/topic', '主题管理'],
+                    ['/views/function/monitor', '运维监控'],
+                    ['/views/function/device', '设备模拟']
+
+                ]
+            },
+            {
+                title: "规则引擎",
+                collapsable: true,
+                sidebarDepth: 1,
+                children: [
+                    ['/views/gzyq/introduce', '规则引擎介绍'],
+                    ['/views/gzyq/eventData', '事件数据'],
+                    ['/views/gzyq/eventTopic', '事件主题'],
+                    ['/views/gzyq/dataBridging', '数据桥接'],
+                    ['/views/gzyq/dataStorage', '数据存储'],
+                    ['/views/gzyq/shakedownTest', '创建规则及调试'],
+                    ['/views/gzyq/grammar', '规则引擎语法'],
+                    ['/views/gzyq/codec', '编解码']
+                ]
+            },
+            ['/views/test.md', '性能压测'],
+            ['/views/vs.md', 'VS EMQX'],
+            ['/views/mqtt', 'MQTT协议介绍']
         ]
-      },
-      {
-        title: "安装部署",
-        collapsable: true,
-        sidebarDepth: 1,
-        children: [
-          ['/views/install/optimize', '系统优化'],
-          ['/views/install/deployment', '部署']
-        ]
-      },
-      ['/views/guide', '配置指南'],
-      {
-        title: "功能",
-        collapsable: true,
-        sidebarDepth: 1,
-        children: [
-          ['/views/function/auth', '认证鉴权'],
-          ['/views/function/control', '访问控制'],
-          ['/views/function/link', '连接管理'],
-          ['/views/function/topic', '主题管理']
-        ]
-      },
-      {
-        title: "规则引擎",
-        collapsable: true,
-        sidebarDepth: 1,
-        children: [
-          ['/views/gzyq/introduce', '规则引擎介绍'],
-          ['/views/gzyq/eventData', '事件数据'],
-          ['/views/gzyq/eventTopic', '事件主题'],
-          ['/views/gzyq/dataBridging', '数据桥接'],
-          ['/views/gzyq/dataStorage', '数据存储'],
-          ['/views/gzyq/shakedownTest', '创建规则及调试'],
-          ['/views/gzyq/grammar', '规则引擎语法'],
-          ['/views/gzyq/codec', '编解码']
-        ]
-      },
-      ['/views/monitor', '运维监测'],
-      ['/views/test.md', '性能压测'],
-      ['/views/vs.md', 'VS EMQX'],
-      ['/views/mqtt', 'MQTT协议介绍']
-    ]
-  }
+    }
 }
---
+++ docs/views/function/device.md
@@ -0,0 +1,33 @@
+# 设备模拟功能
+> FluxMQ提供了一些Web简易版本的设备模拟功能，方便用户进行设备模拟测试，用户可通过设备模拟功能，模拟设备连接，发布消息，订阅消息等操作。
+![](../../assets/images/function/img_48.png)
+
+## 消息发布
+> 提供给集群内的设备直接发布消息，支持选择topic，QoS，Retain等参数。
+> 发布文本选择编码格式，支持字符串、json、Hex等编码格式。
+![](../../assets/images/function/img_49.png)
+
+
+## 云客户端
+> 云客户端提供基于Websocket over MQTT 进行模拟设备连接，用户可通过云客户端，模拟设备连接，发布消息，订阅消息等操作。
+![](../../assets/images/function/img_50.png)
+
+
+### 新建连接
+![](../../assets/images/function/img_51.png)
+
+### 添加订阅
+![](../../assets/images/function/img_52.png)
+![](../../assets/images/function/img_53.png)
+![](../../assets/images/function/img_54.png)
+
+### 取消订阅
+> 选择要操作的Topic 右击选择删除即可
+![](../../assets/images/function/img_55.png)
+### 发布消息
+![](../../assets/images/function/img_56.png)
+
+### 取消连接
+![](../../assets/images/function/img_58.png)
+![](../../assets/images/function/img_57.png)
+    
